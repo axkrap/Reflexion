3a5c63e20a1d15ca3293c435c611ce6862f29aef
Nathan Sweet <nathanjsweet@gmail.com>
2011-03-30 19:15:33 -0700

    second

--- reflexion/reflex/documents/_categories_/php/something/post_db.php
+++
@@ -1,1 +0,0 @@
-<?php $thisDB = array("title"=>'Something', "slug"=>'something', "description"=>'', "template"=>'index', "category"=>'php', "author"=>'Nathan Sweet', "comments"=>true, "pingbacks"=>'', "publish"=>true, "post"=>'asdfasdfasdf', "pingbacks_bool"=>true, "publish_date"=>1299280373);
